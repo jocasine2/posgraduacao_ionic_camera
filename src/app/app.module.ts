5b279ab2c65390cdf92d8966d2cb66d7d7fbc496
Joaquim Campelo da Silva Neto <jcsnetcc@gmail.com>
2022-02-19 16:03:51 -0300

    ✨ Cria acesso a câmera na home

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,12 +6,15 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { CameraService } from './services/camera.service';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
-  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy}
+  , CameraService
+  ],
   bootstrap: [AppComponent],
 })
 export class AppModule {}
